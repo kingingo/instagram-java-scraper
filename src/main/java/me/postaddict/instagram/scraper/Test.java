b7d4c7c52e7ef44b45ca9ef605db8421af2b454c
Felix the King <obenausfelix@googlemail.com>
2019-01-07 20:08:01 +0100

    Update Test.java

--- src/main/java/me/postaddict/instagram/scraper/Test.java
+++ src/main/java/me/postaddict/instagram/scraper/Test.java
@@ -32,7 +32,7 @@ public class Test {
                     .build();
             Instagram client = new Instagram(httpClient);
             client.basePage();
-            client.login("feliixistda", "baker1997");
+            client.login("", "");
 			client.basePage();
 			
 			System.out.println("LOGGED IN");
